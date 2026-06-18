23665010eaff90a68fcf6c3157db2b9403ee1f7f
Keilor Baltodano Martínez <86636398+keilorbm1234@users.noreply.github.com>
2026-06-18 14:16:49 -0600

    Actualización

--- CiudadInteligente/Reporte.cpp
+++ CiudadInteligente/Reporte.cpp
@@ -1,1 +1,34 @@
 #include "Reporte.h"
+#include <iostream>
+
+Reporte::Reporte(string m, string c, string fH) : modulo(m), contenido(c), fechaHora(fH) {}
+
+Reporte::Reporte(const Reporte& otro) : modulo(otro.modulo), contenido(otro.contenido), fechaHora(otro.fechaHora) {}
+
+Reporte& Reporte::operator=(const Reporte& otro) {
+    if (this != &otro) {
+        modulo = otro.modulo; 
+        contenido = otro.contenido;  
+        fechaHora = otro.fechaHora; 
+    }
+   
+    return *this; 
+}
+
+Reporte::~Reporte() {}
+
+string Reporte::getModulo() const {
+    return modulo; 
+}
+
+string Reporte::getContenido() const {
+    return contenido;
+}
+
+string Reporte::getFechaHora() const {
+    return fechaHora; 
+}
+
+void Reporte::mostrar() const {
+    cout << "Modulo: " << modulo << " | Contenido: " << contenido << " | Fecha y hora: " << fechaHora << endl;
+}
--- CiudadInteligente/Reporte.h
+++ CiudadInteligente/Reporte.h
@@ -8,6 +8,14 @@ private:
     string contenido; 
     string fechaHora;
 public: 
-
+    //Esta clase contiene el Big 3 rule (destructor, constructor copia y operador de asignación)
+    Reporte(string m, string c, string fH); 
+    Reporte(const Reporte& otro); 
+    Reporte& operator=(const Reporte& otro); 
+    ~Reporte(); 
+    string getModulo() const; 
+    string getContenido() const;
+    string getFechaHora() const;
+    void mostrar() const; 
 };
 
