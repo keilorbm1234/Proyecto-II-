62e5425434523e187fcc1fed21d298200439e8f8
Keilor Baltodano Martínez <86636398+keilorbm1234@users.noreply.github.com>
2026-06-18 15:43:18 -0600

    Actualizacion

--- CiudadInteligente/SistemaReportes.cpp
+++ CiudadInteligente/SistemaReportes.cpp
@@ -1,3 +1,39 @@
 #include "SistemaReportes.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
+SistemaReportes* SistemaReportes::instancia = nullptr; 
 
+SistemaReportes* SistemaReportes:: getInstance() {
+    if (instancia == nullptr) {
+        instancia = new SistemaReportes(); 
+    }
+    return instancia; 
+}
+
+void SistemaReportes::actualizar(Reporte r) {
+    reportes.push_back(r);
+}
+
+void SistemaReportes::agregarReporte(Reporte r) {
+    reportes.push_back(r);
+}
+
+void SistemaReportes::mostrarTodos() {
+    for (const auto& reporte : reportes) {
+        reporte.mostrar(); 
+    }
+}
+
+void SistemaReportes::mostrarPorModulo(string modulo) {
+    for (const auto& reporte : reportes) {
+        if (reporte.getModulo() == modulo) {
+            reporte.mostrar(); 
+        }
+    }
+}
+
+void SistemaReportes::guardarEnArchivo() {
+
+}
